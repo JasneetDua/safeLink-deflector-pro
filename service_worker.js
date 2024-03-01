7feabedb74470434a06fc8b31fc0170611d2b2d7
Jasneet Dua <jasneetdua96@gmail.com>
2024-03-01 22:13:50 +0530

    added action to decypher whitelist domain links on any page as per need

--- service_worker.js
+++ service_worker.js
@@ -3,3 +3,11 @@ import { refreshNetRequestRules } from './src/modules/c/utils/utils.js';
 chrome.runtime.onInstalled.addListener(function () {
   refreshNetRequestRules();
 });
+
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content.js']
+  });
+});
